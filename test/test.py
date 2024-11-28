493e0f03df604f6eaf642c081ff7890595538cd2
Elwatkinn <2734847727@qq.com>
2024-11-28 17:58:56 +0800

    msg

--- test/test.py
+++ test/test.py
@@ -21,7 +21,6 @@ class Actor(nn.Module):
     
     def forward(self, x):
         return self.actor(x) 
-    
 
 def euler_from_quaternion(quat_angle):
         x = quat_angle[:,0]; y = quat_angle[:,1]; z = quat_angle[:,2]; w = quat_angle[:,3]
@@ -46,7 +45,11 @@ def do_ang(msg_ang):
     ang=msg_ang.twist[2].angular
     ori=msg_ang.pose[2].orientation
 
-dof_map= [2, 0, 1, 5, 3, 4, 8, 6, 7, 11, 9, 10]
+dof_map= [1, 4, 7, 10,
+         2, 5, 8, 11   ,
+          0, 3, 6, 9
+          
+          ]
 j_pos=list()
 j_vec=list()
 def do_joint(msg_joi):
@@ -55,59 +58,78 @@ def do_joint(msg_joi):
     j_vec=list(msg_joi.velocity[dof_map[i]] for i in range(0,12))
 
 
+
 if __name__ == "__main__":
     
     sub_ang=rospy.Subscriber("/gazebo/model_states",gam.ModelStates,do_ang,queue_size=1)
     rospy.init_node("test")
-    mydiv='cpu'
-    velocity_commands=torch.tensor([1,0,0],device=mydiv).unsqueeze(0)
+    mydiv='cuda'
+    velocity_commands=torch.tensor([0.1,0,0],device=mydiv).unsqueeze(0)
 
     sub_joint=rospy.Subscriber("/go1_gazebo/joint_states",sem.JointState,do_joint,queue_size=1)
 
     puber=list()
+    puber.append(rospy.Publisher("/go1_gazebo/FL_calf_controller/command",unm.MotorCmd,queue_size=1))
     puber.append(rospy.Publisher("/go1_gazebo/FL_hip_controller/command",unm.MotorCmd,queue_size=1))
     puber.append(rospy.Publisher("/go1_gazebo/FL_thigh_controller/command",unm.MotorCmd,queue_size=1))
-    puber.append(rospy.Publisher("/go1_gazebo/FL_calf_controller/command",unm.MotorCmd,queue_size=1))
+    puber.append(rospy.Publisher("/go1_gazebo/FR_calf_controller/command",unm.MotorCmd,queue_size=1))
     puber.append(rospy.Publisher("/go1_gazebo/FR_hip_controller/command",unm.MotorCmd,queue_size=1))
     puber.append(rospy.Publisher("/go1_gazebo/FR_thigh_controller/command",unm.MotorCmd,queue_size=1))
-    puber.append(rospy.Publisher("/go1_gazebo/FR_calf_controller/command",unm.MotorCmd,queue_size=1))
+    puber.append(rospy.Publisher("/go1_gazebo/RL_calf_controller/command",unm.MotorCmd,queue_size=1))
     puber.append(rospy.Publisher("/go1_gazebo/RL_hip_controller/command",unm.MotorCmd,queue_size=1))
     puber.append(rospy.Publisher("/go1_gazebo/RL_thigh_controller/command",unm.MotorCmd,queue_size=1))
-    puber.append(rospy.Publisher("/go1_gazebo/RL_calf_controller/command",unm.MotorCmd,queue_size=1))
+    puber.append(rospy.Publisher("/go1_gazebo/RR_calf_controller/command",unm.MotorCmd,queue_size=1))
     puber.append(rospy.Publisher("/go1_gazebo/RR_hip_controller/command",unm.MotorCmd,queue_size=1))
     puber.append(rospy.Publisher("/go1_gazebo/RR_thigh_controller/command",unm.MotorCmd,queue_size=1))
-    puber.append(rospy.Publisher("/go1_gazebo/RR_calf_controller/command",unm.MotorCmd,queue_size=1))
-
-    rate=rospy.Rate(1)
-    rate.sleep()
-    actions=torch.tensor([j_pos],device=mydiv)
     
     model = Actor()
-    weights=torch.load("./source/standalone/workflows/rsl_rl/weight_1/model_3500.pt")
+    weights=torch.load("./model_3500.pt")
     model.load_state_dict(weights['model_state_dict'],strict=False)
+    model.eval()
+    model = model.to(mydiv)
+    
+    rate0=rospy.Rate(1000)
+    targetpos=[-1.3,0.0,0.67,-1.3,0.0,0.67,-1.3,0.0,0.67,-1.3,0.0,0.67]
+    rate0.sleep()
+    startpos=j_pos[:]
+    duaration=1000
+    leg_msg=unm.LowCmd()
+    for i in range(12):
+        leg_msg.motorCmd[i].mode = 10;
+        leg_msg.motorCmd[i].Kp = 180;
+        leg_msg.motorCmd[i].Kd = 8;
+    percent=float(0)
+    for i in range(5000):
+        percent+=float(1/duaration)
+        if percent>1:
+            percent=1
+        for j in range(12):
+            leg_msg.motorCmd[j].q=(1 - percent)*startpos[j] + percent*targetpos[dof_map[j]]
+            puber[dof_map[j]].publish(leg_msg.motorCmd[j])
+        rate0.sleep()
 
+    rate=rospy.Rate(50)
+    actions=torch.tensor(j_pos,device=mydiv).unsqueeze(0)
     while(not rospy.is_shutdown()):
         
         base_ang_vel=torch.tensor([ang.x,ang.y,ang.z],device=mydiv).unsqueeze(0)
         quaternion=torch.tensor([ori.x,ori.y,ori.z,ori.w],device=mydiv).unsqueeze(0)
         roll, pitch, yaw = euler_from_quaternion(quaternion) 
         projected_gravity = torch.stack((roll, pitch, yaw), dim=1)
-        joint_pos=torch.tensor([j_pos],device=mydiv)
-        joint_vec=torch.tensor([j_vec],device=mydiv)
-        
+        joint_pos=torch.tensor(j_pos,device=mydiv).unsqueeze(0)
+        joint_vec=torch.tensor(j_vec,device=mydiv).unsqueeze(0)
         obs=torch.cat([base_ang_vel,projected_gravity,velocity_commands,joint_pos,joint_vec,actions],dim=-1)
         
         output=model(obs)
-
-        leg_msg=unm.LowCmd()
+        print(output)
+        
         for i in range(12):
             leg_msg.motorCmd[i].mode=10
-            leg_msg.motorCmd[i].q=output[i]
-            leg_msg.motorCmd[i].dq=0.1
+            leg_msg.motorCmd[i].q=output[0][i]
             leg_msg.motorCmd[i].dq=0
-            leg_msg.motorCmd[i].Kp=80
-            leg_msg.motorCmd[i].Kd=2
+            leg_msg.motorCmd[i].Kp=20
+            leg_msg.motorCmd[i].Kd=0.5
             leg_msg.motorCmd[i].tau=0
-            puber[i].publish(leg_msg.motorCmd[i])
+            puber[dof_map[i]].publish(leg_msg.motorCmd[i])
         actions=output[:]
         rate.sleep()
